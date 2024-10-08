30bb9cfdbfb6ebf05c0f9ae2d1d9f0bd73cdc8fc
Afonso Paiva <a27915@alunos.ipca.pt>
2024-10-08 11:46:25 +0100

    Final faltam aglms features

--- Calculator/app/src/main/java/com/example/calculator/CalculatorScreen.kt
+++ Calculator/app/src/main/java/com/example/calculator/CalculatorScreen.kt
@@ -7,10 +7,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,6 +19,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import com.example.calculator.ui.theme.colornum
+import com.example.calculator.ui.theme.colorsignal
+import androidx.compose.ui.text.style.TextAlign
 
 
 @Composable
@@ -25,39 +30,60 @@ fun CalculatorScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         var displayText by remember { mutableStateOf("0") }
 
-        Text(text = displayText, modifier= Modifier.padding(28.dp) .fillMaxWidth() .height(64.dp), fontSize = 62.sp, maxLines = 1, overflow = TextOverflow.Ellipsis)
+        Text(
+            text = displayText,
+            modifier = Modifier
+
+                .padding(28.dp)
+                .fillMaxWidth()
+                .height(154.dp),
+            fontSize = 62.sp,
+            maxLines = 1,
+            textAlign = TextAlign.End,
+            overflow = TextOverflow.Ellipsis,
+            color = Color.White
+        )
         for (row in listOf(
             listOf("%", "C", "DEL","/"),
             listOf("7", "8", "9", "x"),
             listOf("4", "5", "6", "-"),
             listOf("1", "2", "3", "+"),
-            listOf("", "0", ",", "="),
+            listOf("", "0", ".", "="),
         )) {
             Row {
                 row.forEach { number ->
+
+
                     Button(onClick = {
 
                         when (number) {
+
+
 
                             "C" -> displayText = "0"
                             "DEL" -> if (displayText.length > 1 && displayText != "Syntax Error" && displayText != "Infinity" ) displayText = displayText.dropLast(1) else displayText = "0"
                             "=" -> displayText = operacao(displayText)
                             else -> {
-                                if (displayText == "0") displayText = number
+                                if (displayText == "0") displayText = if (number == ".") "0." else number
                                 else displayText += number
                             }
                         }
 
+
+
+
+
                     },
-                            modifier = Modifier.weight(1f)
-                                .aspectRatio(1f)
-                                .padding(2.dp)
-                                .height(25.dp)
-                    )
-
-
-                    {
-                        Text(text = number, modifier = Modifier .padding(4.dp) , fontSize = 24.sp)
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(100.dp)
+                            .padding(1.dp),
+                        shape = RoundedCornerShape(8.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = if (number in listOf("%", "C", "DEL", "/", "x", "-", "+", "=")) colorsignal else colornum
+                        )
+                    ) {
+                        Text(text = number, modifier = Modifier.padding(4.dp), fontSize = 22.sp)
                     }
                 }
             }
@@ -71,16 +97,16 @@ fun CalculatorScreen(modifier: Modifier = Modifier) {
 
 fun operacao(expression: String): String {
     return try {
-        // converte virgulas
-        val cleanedExpression = expression.replace(",", ".")
+        val cleanedExpression = expression
 
        //para double
-        val result = cleanedExpression.split(Regex("[+\\-x/]"))
+        val result = cleanedExpression.split(Regex("[+\\-x/%]"))
             .map { it.toDouble() }
 
         // operadoes
-        val operators = cleanedExpression.filter { it == '+' || it == '-' || it == 'x' || it == '/' }
+        val operators = cleanedExpression.filter { it == '+' || it == '-' || it == 'x' || it == '/' || it =='%' }
         var total = result[0]
+
 
 
         for (i in 1 until result.size) {
@@ -89,6 +115,8 @@ fun operacao(expression: String): String {
                 '-' -> total - result[i]
                 'x' -> total * result[i]
                 '/' -> total / result[i]
+                '%' -> total / 100 * result[i]
+                //Este não está a funcionar :(
                 else -> total
             }
         }
--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.calculator.ui.theme.CalculatorTheme
+import com.example.calculator.ui.theme.colorbackground
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +17,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = colorbackground
+                ) { innerPadding ->
                     CalculatorScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
